309d774c0721d5b97046bcfb233422a2b2e228c9
Ravishankar.S <srshankar145@Gmail.com>
2023-06-20 21:01:18 +0530

    Event files
    A small Create Event page.

---
+++ EventManager/eventscript.js
@@ -0,0 +1,12 @@
+const slot = document.getElementById('slot');
+const radioBtns = document.querySelectorAll('input[name="duration"]');
+const fifMins = document.getElementById('15min');
+const thrMins = document.getElementById('30min');
+const sixMins = document.getElementById('60min');
+
+// fifMins.addEventListener('click',)
+
+function optSelected(element) {
+    element.checked = true;
+
+}

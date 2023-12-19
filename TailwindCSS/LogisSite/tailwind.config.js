47fa29632482c2aa5074d7a9cefad40bb6f95c4c
Ravishankar.S <srshankar145@Gmail.com>
2023-12-19 19:35:00 +0530

    Logistics website

---
+++ TailwindCSS/LogisSite/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./public/**/*.html",
+    "./public/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mainInter: ["Inter", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+

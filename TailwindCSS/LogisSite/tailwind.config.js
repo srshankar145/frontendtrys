769c783293244a94d494405f460ee27712073832
Ravishankar.S <srshankar145@Gmail.com>
2023-12-25 21:21:06 +0530

    Modified config

--- TailwindCSS/LogisSite/tailwind.config.js
+++ TailwindCSS/LogisSite/tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
     extend: {
       fontFamily: {
         mainInter: ["Inter", "sans-serif"]
+      },
+      letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.05em',
+        widest: '.1em',
+        widest: '.25em',
+        loosely: '1.0rem'
       }
     },
   },
